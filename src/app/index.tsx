1a1c9d7ecc4fdeb094b336caf6fb90810cbec4d5
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-02-07 18:13:20 -0300

    On the project homepage, I added user interaction to the category buttons using react states

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { View, FlatList } from "react-native";
+import { useState } from "react";
 import { CATEGORIES } from "@/utils/data/products";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={3} />
@@ -11,7 +18,13 @@ export default function Home() {
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <CategoryButton title={item} />}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
         horizontal
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
