527688febc9020b4e6f3aeafbe1f917ced1ea58f
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-02-07 17:57:45 -0300

    I added category buttons component
    I installed the clsx dependency

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,22 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems={3}/>
+      <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <CategoryButton title={item} />}
+        horizontal
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
