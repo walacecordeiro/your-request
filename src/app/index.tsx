7120ee3f4fe92e23030b2af1cd772d907bf312da
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-02-08 18:52:55 -0300

    little adjustments

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { View, FlatList, SectionList, Text } from "react-native";
 import { useState, useRef } from "react";
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { Product } from "@/components/product";
 import { Link } from "expo-router";
 import { useCartStore } from "@/stores/cart-store";
@@ -11,7 +11,7 @@ export default function Home() {
   const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce(
     (total, product) => total + product.quantity,
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -6,17 +6,24 @@ import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
 import { useCartStore } from "@/stores/cart-store";
+import { Redirect } from "expo-router";
 
 export default function Product() {
   const cartStore = useCartStore();
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
-    navigation.goBack();
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -28,6 +35,8 @@ export default function Product() {
       />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
+
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
